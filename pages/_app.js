126ec33261f8a5221b81c6f3268a73dd9d72bde8
diegopuma <diegopumacode@gmail.com>
2021-09-20 15:00:56 -0500

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 
+export function reportWebVitals(metric) {
+  
+  if (metric.label === 'web-vital') {
+    console.log("web-vital")
+    console.log(metric)
+  }
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
